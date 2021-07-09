6c5a33c9f35b024f678515684c69efd0c0ba8bff
Marta Pardo <pardoaraujomarta92@gmail.com>
2021-07-09 17:11:35 +0200

    eigen

--- elipse_eigen.R
+++ elipse_eigen.R
@@ -12,21 +12,6 @@ circle <- function(t, N, mu, sigma){
   vec <- c(x,y)
   return(vec)
 }
-# Function which create an ellipse.
-# For the NGM:
-elipse_NGM <- function(t, N, mu, mu_d, sigma, rho, gamma){
-  x <- (mu_d - mu - (1/gamma)) + (sigma*sqrt(N)*(1+rho))*cos(t)
-  y <- (sigma*sqrt(N)*(1-rho))*sin(t)
-  vec <- c(x,y)
-  return(vec)
-}
-# For the Jacobian:
-elipse_J <- function(t, N, mu, mu_d, sigma, rho, gamma){
-  x <- (mu_d - mu + gamma) + (sigma*sqrt(N)*(1+rho))*cos(t)
-  y <- (sigma*sqrt(N)*(1-rho))*sin(t)
-  vec <- c(x,y)
-  return(vec)
-}
 
 # Function which create a normal random matrix.
 rand_norm_mat <- function(N, mu, sigma){
@@ -34,17 +19,8 @@ rand_norm_mat <- function(N, mu, sigma){
   return(rmatrix)
 }
 
-# Bivariate distribution
-rbvn<-function (n, m1, s1, m2, s2, rho)
-{
-  X1 <- rnorm(n, mu1, s1)
-  X2 <- rnorm(n, mu2 + (s2/s1) * rho *
-                (X1 - mu1), sqrt((1 - rho^2)*s2^2))
-  cbind(X1, X2)
-}
-
 # Function which create a elliptic (bivariate) random matrix.
-rand_ellip_mat <- function(N, mu1, s1,rho){
+rand_ellip_mat_norm <- function(N, mu1, s1,rho){
   mu2 = mu1
   s2 = s1
   mu <- c(mu1,mu2) # Mean
@@ -63,28 +39,71 @@ rand_ellip_mat <- function(N, mu1, s1,rho){
   return(rmatrix)
 }
 
+# Function which create a elliptic (bivariate) random matrix.
+rand_ellip_mat_lognorm <- function(N, mu1, s1,rho){
+  mu2 = mu1
+  s2 = s1
+  mu <- c(mu1,mu2) # Mean
+  sigma <- c(s1*s2*rho, s1*s2*rho) # Covariance matrix
+  rho_mat <- matrix(c(1,rho,rho,1),2)
+  biv_norm <- mvlognormal(n = N^2, Mu = mu, 
+                          Sigma = sigma, R = rho_mat)
+  rmatrix <- matrix(0,N,N)
+  for(i in c(1:N)){
+    for(j in c(i:N)){
+      ind = j + (i-1)*N
+      rmatrix[i,j] = biv_norm[ind,1]
+      rmatrix[j,i] = biv_norm[ind,2]
+    }
+  }
+  
+  return(rmatrix)
+}
 # Function which change the diagonal of a matrix:
-change_diag <- function(N, mat, mu, sigma){
+change_diag_norm <- function(N, mat, mu, sigma){
   diag(mat) <- rnorm(N, mu, sigma)
     return(mat)
 }
 
+change_diag_lognorm <- function(N, mat, mu, sigma){
+  diag(mat) <- rlnorm(N, mu, sigma)
+  return(mat)
+}
+
+# Create the NGM:
 NGM_matrix <- function(N, mu, sigma, mu_d,sigma_d, gam,rho){
-  ellip <- rand_ellip_mat(N, mu, sigma, rho)
+  ellip <- rand_ellip_mat_norm(N, mu, sigma, rho)
   mu_diag <- mu_d - mu
-  mat_1 <- change_diag(N, ellip, mu_diag, sigma_d)
+  mat_1 <- change_diag_norm(N, ellip, mu_diag, sigma_d)
+  mat_2 <- (1/gam)*mat_1
+  return(mat_2)
+}
+
+NGM_matrix_lognorm <- function(N, mu, sigma, mu_d,sigma_d, gam,rho){
+  ellip <- rand_ellip_mat_lognorm(N, mu, sigma, rho)
+  mu_diag <- mu_d - mu
+  mat_1 <- change_diag_lognorm(N, ellip, mu_diag, sigma_d)
   mat_2 <- (1/gam)*mat_1
   return(mat_2)
 }
 
 
+# Create the Jacobian:
 J_matrix <- function(N, mu, sigma, mu_d,sigma_d, gam,rho){
-  ellip <- rand_ellip_mat(N, mu, sigma, rho)
+  ellip <- rand_ellip_mat_norm(N, mu, sigma, rho)
   mu_diag <- mu_d - mu
-  mat_1 <- change_diag(N, ellip, mu_diag, sigma_d)
+  mat_1 <- change_diag_norm(N, ellip, mu_diag, sigma_d)
   mat_2 <- (1/gam)*diag(N) + mat_1
   return(mat_2)
-}
+}  
+
+J_matrix_lognorm <- function(N, mu, sigma, mu_d,sigma_d, gam,rho){
+  ellip <- rand_ellip_mat_lognorm(N, mu, sigma, rho)
+  mu_diag <- mu_d - mu
+  mat_1 <- change_diag_lognorm(N, ellip, mu_diag, sigma_d)
+  mat_2 <- (1/gam)*diag(N) + mat_1
+  return(mat_2)
+}  
 
 # Function which plot the eigenvalues:
 eigen_mat <- function(mat){
@@ -97,7 +116,7 @@ eigen_mat <- function(mat){
 
 
 ############COMPUTATIONS#############
-# Normal distribution:
+# Normal distribution ######
   mu = 0
   sigma = 1
   N = 100
@@ -123,30 +142,18 @@ eigen_mat <- function(mat){
     geom_point(aes(X,Y), size = 0.05, color = "red")+
     geom_point(data = eig, aes(re,im) , size = 0.05)
 
-# Bivariate distribution:
-  mu_bi = 3
-  sigma_bi = 1.2
-  rho = 0.8
-  N = 1000
   
-  mat <- rand_ellip_mat(N,mu_bi,sigma_bi,rho)
-  eig <- eigen_mat(mat)
-  ggplot(eig) +
-    geom_point(aes(re,im), size = 0.05)
-  # +
-  #   xlim(c(-10,10))
-  # 
-  # Diagonal distribution
-  mu_d <- 2
-  mu_diag <- mu_d - mu_bi
-  sigma_d <- 0.4
-  mat <- change_diag(N,mat, mu_diag,sigma_d)
-  eig <- eigen_mat(mat)
-  ggplot(eig) +
-    geom_point(aes(re,im), size = 0.05)
-  # + xlim(c(-20,20))
-
-  gam = 0.2
+# Bivariate distribution #######
+  ## NGM Matrix:
+  mu_bi = 3        # Bivariate mean
+  sigma_bi = 1.2   # Bivariate variance
+  rho = 0.8        # Correlation
+  N = 300           # Size matrix
+  gam = 0.2        # gamma
+  mu_d = 1         # Media diagonal
+  sigma_d = 0.2    # Variance diagonal
+  
+  # with normal distribution:
   mat_2 <- NGM_matrix(N, mu_bi, sigma_bi, mu_d,sigma_d, gam,rho)
   eig <- eigen_mat(mat_2)
   outlier <- data.frame(x = ((mu_d - mu_bi) + mu_bi*N)*(1/gam), y = 0)
@@ -156,35 +163,59 @@ eigen_mat <- function(mat){
                      a = sigma_bi*sqrt(N)*(1+rho)*(1/gam),
                      b = sigma_bi*sqrt(N)*(1-rho)*(1/gam),
                      angle = 0, colour = "red")) +
-    geom_point(data = outlier, aes(x,y), color = "red",size = 0.3)
-
+    geom_point(data = outlier, aes(x,y), color = "red",size = 0.3) +
+    ggtitle("NGM matrix normal distribution") +
+    theme_bw()
+  
+  
+  # with lognormal distribution:
+  mat_2 <- NGM_matrix_lognorm(N, mu_bi, sigma_bi, mu_d,sigma_d, gam,rho)
+  eig <- eigen_mat(mat_2)
+  outlier <- data.frame(x = ((mu_d - mu_bi) + mu_bi*N)*(1/gam), y = 0)
+  plot_J <- ggplot(eig) +
+    geom_point(aes(re,im), size = 0.05) +
+    geom_ellipse(aes(x0 = (mu_d - mu_bi)*(1/gam), y0 = 0,
+                     a = sigma_bi*sqrt(N)*(1+rho)*(1/gam),
+                     b = sigma_bi*sqrt(N)*(1-rho)*(1/gam),
+                     angle = 0, colour = "red")) +
+    geom_point(data = outlier, aes(x,y), color = "red",size = 0.3) +
+    ggtitle("NGM matrix lognormal distribution") +
+    theme_bw()
+  
+  plot_J
+  plot_J + xlim(c(-250,250))
+  
+  ### Jacobian Matrix: 
+  # With normal distribution:
   mat_2 <- J_matrix(N, mu_bi, sigma_bi, mu_d,sigma_d, gam,rho)
   eig <- eigen_mat(mat_2)
-  ggplot(eig) +
-    geom_point(aes(re,im), size = 0.05)
-  
-############# Draw the ellipse  ################:
-  min_seq <- 0
-  max_seq <- 2*pi
-  x <- seq(min_seq, max_seq, 0.01)
-  lap <- lapply(x, elipse_NGM, N, mu, mu_d, sigma, rho, gam)
-  l = length(x)
-  mat <- matrix(unlist(lap,recursive =TRUE), ncol = l, byrow = FALSE)
-  
-  df_mat <- data.frame(t(mat))
-  out <- (mu* sqrt(N) + (mu_d - mu))*gam^(-1)
-  outlier <- c(out,0)
-  df_mat <- rbind(df_mat, outlier)
-  head(df_mat)
-  colnames(df_mat) <- c("X","Y")
-# df_1 <- data.frame(x,vec = -vec)
-# df_join <- rbind(df,df_1)
-  df_center <- data.frame(x = ((mu_d - mu))*gam^(-1), y=0)
-  
-  ggplot(df_mat) +
-    geom_point(aes(X,Y), size = 0.05)+
-    geom_point(data = df_center, aes(x,y), color = "red")
-
-  ggplot(eig) +
+  outlier <- data.frame(x = ((mu_d - mu_bi) + mu_bi*N) + gam, y = 0)
+  plot_J <- ggplot(eig) +
     geom_point(aes(re,im), size = 0.05)+
-    geom_point(data = df_mat,aes(X,Y), size = 0.05, color = "red")
+    geom_ellipse(aes(x0 = (mu_d - mu_bi) +gam, y0 = 0,
+                     a = sigma_bi*sqrt(N)*(1+rho),
+                     b = sigma_bi*sqrt(N)*(1-rho),
+                     angle = 0, colour = "red")) +
+    geom_point(data = outlier, aes(x,y), color = "red",size = 0.3) +
+    ggtitle("Jacobian matrix normal distribution")+
+    theme_bw()
+  
+  plot_J
+  plot_J + xlim(c(-50,50))
+  
+  # with lognormal distribution:
+  mat_2 <- J_matrix_lognorm(N, mu_bi, sigma_bi, mu_d,sigma_d, gam,rho)
+  eig <- eigen_mat(mat_2)
+  outlier <- data.frame(x = ((mu_d - mu_bi) + mu_bi*N) + gam, y = 0)
+  plot_J <- ggplot(eig) +
+    geom_point(aes(re,im), size = 0.05)+
+    geom_ellipse(aes(x0 = (mu_d - mu_bi) +gam, y0 = 0,
+                     a = sigma_bi*sqrt(N)*(1+rho),
+                     b = sigma_bi*sqrt(N)*(1-rho),
+                     angle = 0, colour = "red")) +
+    geom_point(data = outlier, aes(x,y), color = "red",size = 0.3)+
+    ggtitle("Jacobian matrix lognormal distribution")
+    theme_bw()
+  
+  plot_J
+  plot_J + xlim(c(-50,50))
