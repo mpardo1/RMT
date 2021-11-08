bfb1e3c32eace731b34b18f252f85fed92ffa891
mpardo1 <pardoaraujomarta92@gmail.com>
2021-11-08 10:17:35 +0100

    fa

--- low_perturb.R
+++ low_perturb.R
@@ -46,13 +46,13 @@ eigen_mat <- function(mat){
 
 ############COMPUTATIONS#############
 # Normal distribution ######
-mu_bi = 20          # Bivariate mean
-sigma_bi = 10        # Bivariate variance
-N = 100             # Size matrix
+mu_bi = 2          # Bivariate mean
+sigma_bi = 3        # Bivariate variance
+N = 300             # Size matrix
 beta = 1.2          # gamma
 mu_d = 1            # Media diagonal
-sigma_d = 0.0000001 # Variance diagonal
-gam = 1          # beta
+sigma_d = 0.00001 # Variance diagonal
+gam = 3        # beta
 ##--------------------------------------------------------------#
 # NGM:.
 mat_2 <- NGM_matrix(N, mu_bi, sigma_bi, mu_d,sigma_d, beta, gam)
@@ -81,7 +81,7 @@ plot_J <- ggplot(eig) + geom_point(aes(re,im), size = 0.0005)
 plot_J +
   coord_fixed()
 
-center = beta*(1-mu_bi) -gam
+center = beta*(1-mu_bi)-gam
 radius = beta*sigma_bi*sqrt(N)
 outlier = (mu_bi*(N-1)+1)*beta -gam
 
@@ -113,17 +113,24 @@ mat_2 <- J_matrix(N, mu_bi, sigma_bi, mu_d,sigma_d, beta, gam)
 mat_2[1,2] <- rnorm(1,mu_new,sigma_bi)
 eig <- eigen_mat(mat_2)
 
-outlier1 <- (mu_bi*(N/2)+sqrt(mu_bi^2*((N^2/4-1))+mu_bi*mu_new))*beta-gam
-outlier2 <- (mu_bi*(N/2)-sqrt(mu_bi^2*((N^2/4-1))+mu_bi*mu_new))*beta-gam
+outlier1 <- (mu_bi*(N/2)+sqrt(mu_bi^2*((N^2/4-1))+mu_bi*mu_new))
+outlier2 <- (mu_bi*(N/2)-sqrt(mu_bi^2*((N^2/4-1))+mu_bi*mu_new))
+outlier1 <- beta*(outlier1 + (1-mu_bi)) - gam
+outlier2 <- beta*(outlier2 + (1-mu_bi)) - gam
+
 plot_J <- ggplot(eig) + geom_point(aes(re,im), size = 0.05) 
 
 plot_J + 
   geom_circle(aes(x0 = center,
                   y0 = 0,
-                  r = radius, colour = "red")) +
-  geom_point(aes(outlier1,0), colour =  "red") +
-  geom_point(aes(outlier2,0), colour =  "red") +
-  coord_fixed()
+                  r = radius), colour = "red",
+              show.legend = NA,size = 0.2) +
+  geom_point(aes(outlier1,0), colour =  "red",
+             show.legend = NA) +
+  geom_point(aes(outlier2,0), colour =  "red",
+             show.legend = NA) +
+ coord_fixed() +
+  theme_bw() 
 
 # 2. Perturbed flow a single patch and for all (income/outcome) directions.
 mu_new <- 2 # New mean
@@ -131,63 +138,111 @@ mat_2 <- J_matrix(N, mu_bi, sigma_bi, mu_d,sigma_d, beta, gam)
 mat_2[2:N,1] <- rnorm(N-1,mu_new,sigma_bi)
 eig <- eigen_mat(mat_2)
 
-outlier1 <- (mu_bi*(N/2)+sqrt(mu_bi^2*((N^2-2)/4)+mu_bi*mu_new*(N-1)))*beta-gam
-outlier2 <- (mu_bi*(N/2)-sqrt(mu_bi^2*((N^2-2)/4)+mu_bi*mu_new*(N-1)))*beta-gam
+outlier1 <- (mu_bi*(N/2)+sqrt(mu_bi^2*((N^2-2)/4)+mu_bi*mu_new*(N-1)))
+outlier2 <- (mu_bi*(N/2)-sqrt(mu_bi^2*((N^2-2)/4)+mu_bi*mu_new*(N-1)))
+outlier1 <- beta*(outlier1 + (1-mu_bi)) - gam
+outlier2 <- beta*(outlier2 + (1-mu_bi)) - gam
+
 plot_J <- ggplot(eig) + geom_point(aes(re,im), size = 0.05) 
 
 plot_J + 
   geom_circle(aes(x0 = center,
                   y0 = 0,
-                  r = radius, colour = "red")) +
-  geom_point(aes(outlier1,0), colour =  "red") +
-  geom_point(aes(outlier2,0), colour =  "red") +
-  coord_fixed()
+                  r = radius),size = 0.2 , colour = "red",
+              show.legend = NA) +
+  geom_point(aes(outlier1,0), colour =  "blue",
+             show.legend = NA) +
+  geom_point(aes(outlier2,0), colour =  "green",
+             show.legend = NA) +
+  coord_fixed() +
+  theme_bw() 
 
 # 3. Perturbed flow for one single flow in both directions.
-mu_new <- 2 # New mean
+# N <- 700
+N <- 300 
+mu_bi <- 3
+beta <- 2
+gam <- 4
+mu_new <- 3 # New mean
+
+center = beta*(1-mu_bi)-gam
+radius = beta*sigma_bi*sqrt(N)
 mat_2 <- J_matrix(N, mu_bi, sigma_bi, mu_d,sigma_d, beta, gam)
 mat_2[2,1] <- rnorm(1,mu_new,sigma_bi)
 mat_2[1,2] <- rnorm(1,mu_new,sigma_bi)
 eig <- eigen_mat(mat_2)
 
-outlier1 <- (mu_bi*((N-1)/2)+mu_new/2+sqrt(mu_bi^2*((N^2+2*N-7)/4)-mu_bi*mu_new*(N-3)/2)+(mu_new^2/4))*beta-gam
-outlier2 <- (mu_bi*((N-1)/2)+mu_new/2-sqrt(mu_bi^2*((N^2+2*N-7)/4)-mu_bi*mu_new*(N-3)/2)+(mu_new^2/4))*beta-gam
+outlier1 <- (mu_bi*((N-1)/2)+mu_new/2+sqrt(mu_bi^2*((N^2+2*N-7)/4)+mu_bi*mu_new*(N-3)/2)+(mu_new^2/4))
+outlier2 <- (mu_bi*((N-1)/2)+mu_new/2-sqrt(mu_bi^2*((N^2+2*N-7)/4)+mu_bi*mu_new*(N-3)/2)+(mu_new^2/4))
 outlier3 <- mu_bi - mu_new
+outlier1 <- beta*(outlier1 + (1-mu_bi)) - gam
+outlier2 <- beta*(outlier2 + (1-mu_bi)) - gam
+outlier3 <- beta*(outlier3 + (1-mu_bi)) - gam
+
 plot_J <- ggplot(eig) + geom_point(aes(re,im), size = 0.05) 
 
 plot_J + 
   geom_circle(aes(x0 = center,
                   y0 = 0,
-                  r = radius, colour = "red")) +
-  geom_point(aes(outlier1,0), colour =  "red") +
-  geom_point(aes(outlier2,0), colour =  "red") +
-  geom_point(aes(outlier3,0), colour =  "red") +
-  coord_fixed()
+                  r = radius), size = 0.2 ,colour = "red",
+              show.legend = NA) +
+  geom_point(aes(outlier1,0), colour =  "blue",
+             show.legend = NA) +
+  geom_point(aes(outlier2,0), colour =  "forestgreen",
+             show.legend = NA) +
+  geom_point(aes(outlier3,0), colour =  "purple",
+             show.legend = NA) +
+  coord_fixed() + theme_bw()
 
 # 4. Perturbed for two patches and for all (income/outcome) directions.
-mu_new <- 2 # New mean
+N <- 300 
+mu_bi <- 3
+beta <- 2
+gam <- 2
+mu_new <- 4 # New mean
+
+center = beta*(1-mu_bi)-gam
+radius = beta*sigma_bi*sqrt(N)
+
 mat_2 <- J_matrix(N, mu_bi, sigma_bi, mu_d,sigma_d, beta, gam)
 mat_2[2:N,1] <- rnorm(N-1,mu_new,sigma_bi)
 mat_2[1,2] <- rnorm(1,mu_new,sigma_bi)
 mat_2[3:N,2] <- rnorm(N-2,mu_new,sigma_bi)
 eig <- eigen_mat(mat_2)
 
-outlier1 <- (mu_bi*((N-1)/2)+mu_new/2+sqrt(mu_bi^2*((N-3)^2/4)-mu_bi*mu_new*(3*N-5)/2)+(mu_new^2/4))*beta-gam
-outlier2 <- (mu_bi*((N-1)/2)+mu_new/2-sqrt(mu_bi^2*((N-3)^2/4)-mu_bi*mu_new*(3*N-5)/2)+(mu_new^2/4))*beta-gam
-outlier3 <- mu_bi - mu_new
+outlier1 <- (mu_bi*((N-1)/2)+mu_new/2+sqrt(mu_bi^2*((N-3)^2/4)+mu_bi*mu_new*(3*N-5)/2)+(mu_new^2/4))
+outlier2 <- (mu_bi*((N-1)/2)+mu_new/2-sqrt(mu_bi^2*((N-3)^2/4)+mu_bi*mu_new*(3*N-5)/2)+(mu_new^2/4))
+outlier3 <- (mu_bi - mu_new)
+outlier1 <- beta*(outlier1 + (1-mu_bi)) - gam
+outlier2 <- beta*(outlier2 + (1-mu_bi)) - gam
+outlier3 <- beta*(outlier3 + (1-mu_bi)) - gam
+
+
 plot_J <- ggplot(eig) + geom_point(aes(re,im), size = 0.05) 
 
 plot_J + 
   geom_circle(aes(x0 = center,
                   y0 = 0,
-                  r = radius, colour = "red")) +
-  geom_point(aes(outlier1,0), colour =  "red") +
-  geom_point(aes(outlier2,0), colour =  "red") +
-  geom_point(aes(outlier3,0), colour =  "red") +
-  coord_fixed()
+                  r = radius),size = 0.2 , colour = "red",
+              show.legend = NA) +
+  geom_point(aes(outlier1,0), colour =  "blue",
+             show.legend = NA) +
+  geom_point(aes(outlier2,0), colour =  "forestgreen",
+             show.legend = NA) +
+  geom_point(aes(outlier3,0), colour =  "purple",
+             show.legend = NA) +
+  coord_fixed() + theme_bw()
 
 # 5. Perturbed for three patches and for all (income/outcome) directions.
-mu_new <- 2 # New mean
+N <- 400 
+mu_bi <- 3
+beta <- 2
+gam <- 2
+mu_new <- 4 # New mean
+
+center = beta*(1-mu_bi)-gam
+radius = beta*sigma_bi*sqrt(N)
+
 mat_2 <- J_matrix(N, mu_bi, sigma_bi, mu_d,sigma_d, beta, gam)
 mat_2[2:N,1] <- rnorm(N-1,mu_new,sigma_bi)
 mat_2[1,2] <- rnorm(1,mu_new,sigma_bi)
@@ -196,23 +251,40 @@ mat_2[1:2,3] <- rnorm(2,mu_new,sigma_bi)
 mat_2[4:N,3] <- rnorm(N-3,mu_new,sigma_bi)
 eig <- eigen_mat(mat_2)
 
-outlier1 <- (mu_bi*((N-2)/2)+mu_new+sqrt(mu_bi^2*((N-4)^2/4)-mu_bi*mu_new*(3*N-5))+(mu_new^2))*beta-gam
-outlier2 <- (mu_bi*((N-2)/2)+mu_new-sqrt(mu_bi^2*((N-4)^2/4)-mu_bi*mu_new*(3*N-5))+(mu_new^2))*beta-gam
+outlier1 <- (mu_bi*((N-2)/2)+mu_new+sqrt(mu_bi^2*((N-4)^2/4)+mu_bi*mu_new*(3*N-5))+(mu_new^2))
+outlier2 <- (mu_bi*((N-2)/2)+mu_new-sqrt(mu_bi^2*((N-4)^2/4)+mu_bi*mu_new*(3*N-5))+(mu_new^2))
 outlier3 <- mu_bi - mu_new
+outlier1 <- beta*(outlier1 + (1-mu_bi)) - gam
+outlier2 <- beta*(outlier2 + (1-mu_bi)) - gam
+outlier3 <- beta*(outlier3 + (1-mu_bi)) - gam
+
+
 plot_J <- ggplot(eig) + geom_point(aes(re,im), size = 0.05) 
 
 plot_J + 
   geom_circle(aes(x0 = center,
                   y0 = 0,
-                  r = radius, colour = "red")) +
-  geom_point(aes(outlier1,0), colour =  "red") +
-  geom_point(aes(outlier2,0), colour =  "red") +
-  geom_point(aes(outlier3,0), colour =  "red") +
-  coord_fixed()
+                  r = radius),size = 0.2 , colour = "red",
+              show.legend = NA) +
+  geom_point(aes(outlier1,0), colour =  "blue",
+             show.legend = NA) +
+  geom_point(aes(outlier2,0), colour =  "forestgreen",
+             show.legend = NA) +
+  geom_point(aes(outlier3,0), colour =  "purple",
+             show.legend = NA) +
+  coord_fixed() + theme_bw()
 
 
 # 6. Perturbed for four patches and for all (income/outcome) directions.
-mu_new <- 2 # New mean
+N <- 200 
+mu_bi <- 3
+beta <- 2
+gam <- 2
+mu_new <- 4 # New mean
+
+center = beta*(1-mu_bi)-gam
+radius = beta*sigma_bi*sqrt(N)
+
 mat_2 <- J_matrix(N, mu_bi, sigma_bi, mu_d,sigma_d, beta, gam)
 mat_2[2:N,1] <- rnorm(N-1,mu_new,sigma_bi)
 mat_2[1,2] <- rnorm(1,mu_new,sigma_bi)
@@ -223,19 +295,27 @@ mat_2[1:3,4] <- rnorm(3,mu_new,sigma_bi)
 mat_2[5:N,4] <- rnorm(N-4,mu_new,sigma_bi)
 eig <- eigen_mat(mat_2)
 
-outlier1 <- (mu_bi*((N-3)/2)+(3/2)*mu_new+sqrt(mu_bi^2*((N-5)^2/4)-mu_bi*mu_new*(5*N-17))+((9/4)*mu_new^2))*beta-gam
-outlier2 <- (mu_bi*((N-3)/2)+(3/2)*mu_new-sqrt(mu_bi^2*((N-5)^2/4)-mu_bi*mu_new*(5*N-17))+((9/4)*mu_new^2))*beta-gam
+outlier1 <- (mu_bi*((N-3)/2)+(3/2)*mu_new+sqrt(mu_bi^2*((N-5)^2/4)+mu_bi*mu_new*(5*N-17))+((9/4)*mu_new^2))
+outlier2 <- (mu_bi*((N-3)/2)+(3/2)*mu_new-sqrt(mu_bi^2*((N-5)^2/4)+mu_bi*mu_new*(5*N-17))+((9/4)*mu_new^2))
 outlier3 <- mu_bi - mu_new
+outlier1 <- beta*(outlier1 + (1-mu_bi)) - gam
+outlier2 <- beta*(outlier2 + (1-mu_bi)) - gam
+outlier3 <- beta*(outlier3 + (1-mu_bi)) - gam
+
 plot_J <- ggplot(eig) + geom_point(aes(re,im), size = 0.05) 
 
 plot_J + 
   geom_circle(aes(x0 = center,
                   y0 = 0,
-                  r = radius, colour = "red")) +
-  geom_point(aes(outlier1,0), colour =  "red") +
-  geom_point(aes(outlier2,0), colour =  "red") +
-  geom_point(aes(outlier3,0), colour =  "red") +
-  coord_fixed()
+                  r = radius),size = 0.2 , colour = "red",
+              show.legend = NA) +
+  geom_point(aes(outlier1,0), colour =  "blue",
+             show.legend = NA) +
+  geom_point(aes(outlier2,0), colour =  "forestgreen",
+             show.legend = NA) +
+  geom_point(aes(outlier3,0), colour =  "purple",
+             show.legend = NA) +
+  coord_fixed() + theme_bw()
 
 # 7. Change trnasmision rates.
 mu_new <- 2 # New mean
