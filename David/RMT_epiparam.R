83f74b66851d6a448ec43e4a22a65bbd2f85c24d
mpardo1 <pardoaraujomarta92@gmail.com>
2022-04-06 10:34:24 +0200

    asd

---
+++ David/RMT_epiparam.R
@@ -0,0 +1,74 @@
+####### RANDOM MATRICES FOR METAPOPULATION MODELS #######
+### 
+### parent script
+###
+### generate, plot and integrate metapopulation
+### epidemiological models
+### 
+rm(list = ls())
+source("~/RMT/David/RMT_genrandom.R")
+source("~/RMT/David/RMT_plotmobility.R")
+source("~/RMT/David/d_functions_eigen_int.R")
+
+####### GENERATE JACOBIAN ###############################
+# number of patches
+N <- 100
+
+# epidemiological
+#all rates must lie in (0,1) except for betas
+
+Deltas <- rep(0.3, N) # birth rate
+mub <- 0.02
+sb <- 0.001
+betas <- rep(mub, N) # transmission rates
+# betas <- rgamma(N, shape = (mub/sb)^2, rate = mub/(sb^2))
+thetas <- rep(0.3, N) # loss of immunity rates
+mud <- 0.3
+deaths <- rep(mud, N) # not disease-related death rates
+mua <- 0.2
+alphas <- rep(mua, N) # recovery rates
+mudel <- 0
+deltas <- rep(mudel, N) # disease-related death rates
+gammas = deaths + alphas + deltas
+
+# mobility
+#commuting and migration networks
+muw <- 0.2 
+sw <- 0.05
+rhow <- 0 #original rho (Gamma of baron et al)
+Gammaw <- 0 #gamma of baron et al
+rw <- 0
+cw <- 0
+
+muc <- 0.001
+sc <- 0.0001
+rhoc <- 0
+Gammac <- 0
+rc <- 0
+cc <- 0
+
+COMMUTING <- rand_mat(N, muw, sw, distrib = "beta")
+diag(COMMUTING) <- 0
+# COMMUTING <- rand_mat_ell(N, muw, sw, rhow, distrib = "beta")
+# COMMUTING[sample.int(N^2, round(p*N^2))] <- 0
+
+MIGRATION <- rand_mat(N, muc, sc, distrib = "beta")
+diag(MIGRATION) <- 0
+
+alp_bet_vec <- seq(0,2.5,0.01)
+end_time <- 50
+sol <- int(N, Deltas,betas,deaths,thetas,alphas,deltas,
+           COMMUTING,MIGRATION,
+           sus_init,inf_init,end_time)
+df_sum <- data.frame(time = sol[,1])
+for(i in c(1:length(alp_bet_vec))){
+  print(paste0("i: ", i))
+  betas <- rep(mub, N) 
+  betas[1] <- betas[1] + alp_bet_vec[i]
+  sol <- int(N, Deltas,betas,deaths,thetas,alphas,deltas,
+             COMMUTING,MIGRATION,
+             sus_init,inf_init,end_time)
+  sol_inf <- sol[,c(1,(N+2):(2*N+1))]
+  sum_inf <- rowSums(sol_inf[,2:(N+1)])
+  df_sum[,ncol(df_sum)+1] <- sum_inf
+}
