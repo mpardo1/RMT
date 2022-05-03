ed743de3bc89f21e4b3ea217f28eb2af8f716926
mpardo1 <pardoaraujomarta92@gmail.com>
2022-05-03 10:55:24 +0200

    asd

--- David/RMT_corr_BG.R
+++ David/RMT_corr_BG.R
@@ -15,14 +15,14 @@ source("~/RMT/David/d_functions_eigen_int.R")
 ####### GENERATE JACOBIAN ###############################
 
 # number of patches
-N <- 4000
+N <- 400
 
 muw <- 1
 sw <- 0.7
-rhow <- -0.4 #original rho (Gamma of baron et al)
-Gammaw <- 0.7 #gamma of baron et al
-rw <- 1.4
-cw <- 1.6
+rhow <- 0.4 #original rho (Gamma of baron et al)
+Gammaw <- 0.1 #gamma of baron et al
+rw <- 0.4
+cw <- 0.3
 
 (Gammaw/sqrt(rw*cw) < 1) & ((N*rhow-2*Gammaw)/(N-(rw+cw)) < 1)
 # Outliers:
@@ -55,8 +55,8 @@ center <-  -1
 radius <- sw
 ggplot(eig_MPA) + 
   geom_point(aes(re, im), size = 0.1) + 
-  geom_circle(aes(x0=outl_BG,y0=0, r = 0.01), colour = "red") +
-  geom_circle(aes(x0=outl,y0=0, r = 0.01), colour = "green") +
+  geom_circle(aes(outl_BG,0), colour = "red") +
+  geom_point(aes(outl,0), colour = "green") +
   geom_ellipse(aes(x0 = center, y0 = 0, a = (1+rhow)*radius,
                    b = (1-rhow)*radius, angle = 0), color = "red") 
 
@@ -71,8 +71,7 @@ plot_eig +
 plot_eig + 
   geom_point(aes(outl_BG,0), colour = "green") +
   geom_point(aes(outl,0), colour = "blue", size= 0.2) +
-  theme_bw() + 
-  xlim(c(0.9,1.1))
+  theme_bw() 
 
 eig_DD <- eigen_mat(com_mat_D[[1]])
 eig_DD_circ <- eig_DD[-which(eig_DD$re == max(eig_DD$re)),]
@@ -84,3 +83,16 @@ ggplot(eig_DD_circ) +
   geom_ellipse(aes(x0 = center, y0 = 0, a = (1+rhow)*radius,
                    b = (1-rhow)*radius, angle = 0), color = "red") 
   
+# Reescale variables:
+com_mat_resc <- rand_mat_cor_norm_MPA_resc(N,muw,sw,rhow,Gammaw,rw,cw)
+plot_eig <- plot_eigen(com_mat_resc[[1]])
+com_mat_resc[[2]]
+
+outl_BG <- -1 + muw + (muw/2)*(1+rhow/Gammaw)*(sqrt(1+(4*Gammaw*sw^2/muw^2))-1)
+outl_BG <- outl_BG*N^2
+outl <- -1 + muw + rhow*sw^2/muw
+
+plot_eig + 
+  geom_point(aes(outl_BG,0), colour = "green") +
+  geom_point(aes(outl,0), colour = "blue") +
+  theme_bw()
--- David/RMT_genrandom_1.R
+++ David/RMT_genrandom_1.R
@@ -256,7 +256,7 @@ rand_mat_cor_norm_MPA <- function(N,mu,sig,rho,G,r,c){
   cond2 <- ifelse(c<0, FALSE, TRUE)
   cond3 <- ifelse(abs(G) > sqrt(r*c), FALSE, TRUE)
   cond4 <- ifelse(r+c > N, FALSE, TRUE)
-  cond5 <- ifelse(abs(rho - (2*G/N)) > (1-((r+c)/N)), FALSE, TRUE)
+  cond5 <- ifelse(abs(N*rho - (2*G)) > (N-((r+c))), FALSE, TRUE)
   
   if(cond1 == TRUE & cond2 == TRUE & cond3 == TRUE & cond4 == TRUE & cond5 == TRUE ){
     print("Conditions hold")
@@ -311,6 +311,71 @@ rand_mat_cor_norm_MPA <- function(N,mu,sig,rho,G,r,c){
   
   return(list(rmatrix,paramdf))
 }
+
+# Rand matrix MPA reescale!
+rand_mat_cor_norm_MPA_resc <- function(N,mu,sig,rho,G,r,c){
+  
+  cond1 <- ifelse(r<0, FALSE, TRUE)
+  cond2 <- ifelse(c<0, FALSE, TRUE)
+  cond3 <- ifelse(abs(G) > sqrt(r*c), FALSE, TRUE)
+  cond4 <- ifelse((r+c) > 1, FALSE, TRUE)
+  cond5 <- ifelse(abs(rho - (2*G)) > (1-(r+c)), FALSE, TRUE)
+  
+  if(cond1 == TRUE & cond2 == TRUE & cond3 == TRUE & cond4 == TRUE & cond5 == TRUE ){
+    print("Conditions hold")
+  }else{
+    print("Conditions does not hold")
+  }
+  
+  # The param in normalCopula gives the correlation between the variables.
+  # Inside list there are the mean and the standard deviation, ie, sigma, 
+  # not the var = sig^2.
+  copfc <- normalCopula(param=G/sqrt(r*c), dim = 2, dispstr = "un")
+  bivfc <- mvdc(copula = copfc, margins=c("norm","norm"),
+                paramMargins=list(list(0,sig*sqrt(r)),
+                                  list(0,sig*sqrt(c))))
+  
+  valfc <- rMvdc(N, bivfc)
+  
+  fils <- valfc[,1] #betas of baron et al
+  cols <- valfc[,2] #kappas of baron et al
+  
+  copmob <- normalCopula(param= (rho-(2*G))/(1-(r+c)), dim = 2, dispstr = "un")
+  bivmob <- mvdc(copula = copmob, margins=c("norm","norm"),
+                 paramMargins=list(list(0, sig*sqrt(1-(r+c))),
+                                   list(0, sig*sqrt(1-(r+c)))))
+  
+  valmob <- rMvdc(N*(N-1)/2, bivmob)
+  
+  rmatrix <- matrix(0,N,N)
+  ind <- 1
+  for(i in c(1:(N-1))){
+    for(j in c((i+1):N)){
+      # rmatrix[i,j] = mu/N + valmob[ind,1] + fils[i] + cols[j]
+      # rmatrix[j,i] = mu/N + valmob[ind,2] + fils[j] + cols[i]
+      rmatrix[i,j] = valmob[ind,1] + fils[i] + cols[j]
+      rmatrix[j,i] = valmob[ind,2] + fils[j] + cols[i]
+      ind <- ind+1
+    }
+  }
+  rmatrix <-  rmatrix + mu
+  diag(rmatrix) <- mu - 1
+  
+  paramdf <- data.frame(wantedN = c(mu,sig^2,rho,G,r,c),
+                        simulated = c(mean(rmatrix),
+                                      var(fils)+var(cols)+var(valmob[,1]),
+                                      (2*cov(fils,cols)+cov(valmob[,1],
+                                                            valmob[,2]))/(var(fils)+var(cols)+var(valmob[,1])),
+                                      cov(fils,cols)/(var(fils)+var(cols)+var(valmob[,1])),
+                                      var(fils)/(var(fils)+var(cols)+var(valmob[,1])),
+                                      var(cols)/(var(fils)+var(cols)+var(valmob[,1])))) %>%
+    mutate(simulated = round(simulated,6))
+  rownames(paramdf) <- c("mu","sigma","rho","Gamma","r","c")
+  
+  return(list(rmatrix,paramdf))
+}
+
+
 
 #### SAVE ANTES DE ENREDAR CON BARON GALLA
 
