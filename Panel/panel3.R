fab139b5aa9cbf84b2997c852339ab312d37c28b
Marta Pardo <pardoaraujomarta92@gmail.com>
2022-06-30 18:53:43 +0200

    sad

--- Panel/panel3.R
+++ Panel/panel3.R
@@ -5,7 +5,7 @@
 ### generate, plot and integrate metapopulation
 ### epidemiological models
 ### 
-# rm(list = ls())
+rm(list = ls())
 source("~/RMT/David/RMT_genrandom.R")
 source("~/RMT/David/RMT_plotmobility.R")
 source("~/RMT/David/d_functions_eigen_int.R")
@@ -13,20 +13,17 @@ source("~/RMT/David/d_functions_eigen_int.R")
 ####### GENERATE JACOBIAN ###############################
 
 # number of patches
-N <- 100
+N <- 50
 
 # epidemiological
 #all rates must lie in (0,1) except for betas
 
-Deltas <- rep(0.3, N) # birth rate
-mub <- 0.02
-sb <- 0.001
-betas <- rep(mub, N) # transmission rates
+Deltas <- rep(0.6, N) # birth rate
 # betas <- rgamma(N, shape = (mub/sb)^2, rate = mub/(sb^2))
-thetas <- rep(0.3, N) # loss of immunity rates
-mud <- 0.3
+thetas <- rep(0.4, N) # loss of immunity rates
+mud <- 0.6
 deaths <- rep(mud, N) # not disease-related death rates
-mua <- 0.3
+mua <- 0.45
 alphas <- rep(mua, N) # recovery rates
 mudel <- 0
 deltas <- rep(mudel, N) # disease-related death rates
@@ -34,15 +31,15 @@ gammas = deaths + alphas + deltas
 
 # mobility
 #commuting and migration networks
-muw <- 0.5
-sw <- 0.2
+muw <- 0.6
+sw <- 0.3
 rhow <- 0 #original rho (Gamma of baron et al)
 Gammaw <- 0 #gamma of baron et al
 rw <- 0
 cw <- 0
 
-muc <- 0.001
-sc <- 0.0001
+muc <- 0.01
+sc <- 0.002
 rhoc <- 0
 Gammac <- 0
 rc <- 0
@@ -56,26 +53,20 @@ diag(COMMUTING) <- 0
 MIGRATION <- rand_mat(N, muc, sc, distrib = "beta")
 diag(MIGRATION) <- 0
 ##### Random beta ####
-mub <- 0.5
-sb <- 0.3
+mub <- 0.3
+sb <- 0.4
 betas <- rgamma(N, shape = (mub/sb)^2, rate = mub/(sb^2)) 
-betas <- runif(N, 0,1) 
+
 jacobian <- (COMMUTING + diag(N)) %*% diag(betas) + MIGRATION -
   diag(deaths + alphas + deltas + colSums(MIGRATION))
 
 mub <- mean(betas)
-plot_eigen_rmt(jacobian,
-               N,mub,mug = mud + mua + mudel,
-               muw,sw,rhow,Gammaw,
-               muc,sc,rhoc,Gammac,
-               tau = 0, alp = 0, K = 0) +
-  scale_y_continuous( breaks=c(0)) 
 
 print(plot_eigen(jacobian))
 
-sus_init <- rep(10, N) # Initial susceptibles
-inf_init <- rep(10, N)
-end_time <- 100
+sus_init <- rep(100, N) # Initial susceptibles
+inf_init <- rep(100, N)
+end_time <- 40
 sol.rand <- int(N, Deltas,betas,deaths,thetas,alphas,deltas,
            COMMUTING,MIGRATION,
            sus_init,inf_init,end_time)
@@ -115,7 +106,7 @@ plot_stab.rand  <- ggplot(df_inf) +
                         labels=c(0,0.5,1),
                         limits=c(0,1) )  +  theme_bw()
 
-plot_stab.rand
+plot_stab.rand + xlim(c(0,3))
 
 ###### Mean(rand(betas)) ######
 betas_cte <- rep(mean(betas),N)
@@ -133,7 +124,7 @@ plot_eigen_rmt(jacobian,
 eig <- eigen_mat(jacobian)
 max(eig$re)
 
-# print(plot_eigen(jacobian))
+print(plot_eigen(jacobian))
 
 sol.mean <- int(N, Deltas,betas_cte,deaths,thetas,alphas,deltas,
            COMMUTING,MIGRATION,
@@ -148,22 +139,23 @@ library("ggpubr")
 text_size <- 15
 plot1 <- plot_stab.rand +
   xlim(c(0,10))  +
-         scale_y_continuous(breaks=c(0, 200, 400,600,800))  + 
+         # scale_y_continuous(breaks=c(0, 200, 400,600,800))  + 
   ylab("Infected infividuals") +
   theme_bw()+
   theme(text = element_text(size = text_size)) 
  
 vec_col <-  vector(mode="character", length=N)
-vec_col[1:N] <- "#7018D5"
+vec_col[1:N] <- "#9821B3"
  
 plot2 <- plot_stab.mean +
   xlim(c(0,10))  + 
   scale_colour_manual(values = vec_col) +
   ylab("Infected infividuals") +
-  scale_y_continuous(breaks=c(0, 200, 400,600,800)) +
+  # scale_y_continuous(breaks=c(0, 200, 400,600,800)) +
   theme(text = element_text(size = text_size)) 
 
-gg_comp <- ggarrange(plot1,plot2,
+gg_comp <- ggarrange(plot1,
+                     plot2 + ylab(""),
                      common.legend = TRUE, legend = "bottom",
                      labels = c("a", "b"))
 gg_comp
